9c89fb79724bb6081d5a06256ea47b62d422478a
jsanders1999 <julian.arthur.sanders@gmail.com>
2023-05-12 14:48:43 +0200

    Wrong index for order argument

--- main_gmres_poisson.cpp
+++ main_gmres_poisson.cpp
@@ -77,9 +77,9 @@ int main(int argc, char* argv[])
 
   if      (argc==1) {nx=64;            ny=64;            nz=64;            order = 2;}
   else if (argc==2) {nx=atoi(argv[1]); ny=nx;            nz=nx;            order = 2;}
-  else if (argc==3) {nx=atoi(argv[1]); ny=nx;            nz=nx;            order = atoi(argv[1]);}
+  else if (argc==3) {nx=atoi(argv[1]); ny=nx;            nz=nx;            order = atoi(argv[2]);}
   else if (argc==4) {nx=atoi(argv[1]); ny=atoi(argv[2]); nz=atoi(argv[3]); order = 2;         }
-  else if (argc==5) {nx=atoi(argv[1]); ny=atoi(argv[2]); nz=atoi(argv[3]); order = atoi(argv[1]);}
+  else if (argc==5) {nx=atoi(argv[1]); ny=atoi(argv[2]); nz=atoi(argv[3]); order = atoi(argv[2]);}
   else {std::cerr << "Invalid number of arguments (should be 0, 1, 2 or 3, or 4)"<<std::endl; exit(-1);}
   if (ny<0) ny=nx;
   if (nz<0) nz=nx;
